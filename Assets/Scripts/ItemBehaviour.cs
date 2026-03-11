23209e51679c60c2bc97f08d18e867776fa190ec
At0nic <Honzikmelichar01@gmail.com>
2026-03-11 19:16:20 +0100

    4 ItemBehaviour Polish (Prefab Start)

--- Assets/Scripts/ItemBehaviour.cs
+++ Assets/Scripts/ItemBehaviour.cs
@@ -2,11 +2,20 @@ using UnityEngine;
 
 public class ItemBehaviour : MonoBehaviour
 {
-    public string ItemType;
+    //Information
+    public string itemType;
+    public int ammoCount;
+    public float timeFromLastPickup;
+    
+    //Conditions
     public bool isFlying = false;
     public bool onGround = true;
     private bool playerInRange = false;
     public bool playerPickedUp = false;
+    
+    //Script Reference
+    private Shooting shooting;
+    
     
     // Update is called once per frame
     void Update()
@@ -24,15 +33,21 @@ public class ItemBehaviour : MonoBehaviour
             onGround = false;
             Debug.Log("Item Picked Up!");
 
+            timeFromLastPickup = Time.time;
+            shooting.currentItem = itemType;
+            shooting.itemBehaviour = this;
+
             Destroy(gameObject); //Destroys GO and stores data in other GO
         }
     }
+    
     
     //System for Player In Range Detection with Item's Collider
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
             playerInRange = true;
+        shooting = other.GetComponent<Shooting>();
     }
     private void OnTriggerExit2D(Collider2D other)
     {
--- Assets/Scripts/Player/Logic/Shooting.cs
+++ Assets/Scripts/Player/Logic/Shooting.cs
@@ -2,47 +2,82 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
+    //Unity
     public Transform firePoint;
     public GameObject bulletPrefab;
-    public string GunType;      // AR/SMG, Pistol, Shotgun
+    
+    //Info
+    public string currentItem;      // AR/SMG, Pistol, Shotgun
     public bool isFiring;
+    public bool pickingUp;
+    public float bulletForce = 20f;
+    public ItemBehaviour itemBehaviour;
     
     //Shooting Intervals
     private float timeOfLastShot;
     private double timeBetweenShots;
     
-    public float bulletForce = 20f;
-
+    void Start()
+    {
+        itemBehaviour = GetComponent<ItemBehaviour>();
+    }
+    
     // Update is called once per frame
     void Update()
     {
-        if (GunType == "shotgun")
+        /*if (Input.GetButtonDown("Fire2") && currentItem != "" && pickingUp == false)
+        {
+            Debug.Log("Item thrown!");
+            currentItem = "";
+        }
+        else if (Input.GetButtonDown("Fire2") && currentItem == "")
+        {
+            Debug.Log("Item not in hand!");
+        }*/
+
+        if (Input.GetButtonDown("Fire2") && Time.time - itemBehaviour.timeFromLastPickup > 0.05)
+        {
+            if (currentItem != "" && pickingUp == false)
+            {
+                Debug.Log("Item thrown!");
+                currentItem = "";
+            }
+            else if (currentItem == "")
+            {
+                Debug.Log("Item not in hand!");
+            }
+        }
+        
+        if (currentItem == "shotgun")
         {
             timeBetweenShots = 1;
-            if (Input.GetButtonDown("Fire1") && Time.time - timeOfLastShot > timeBetweenShots)
+            if (Input.GetButtonDown("Fire1") && Time.time - timeOfLastShot > timeBetweenShots &&  itemBehaviour.ammoCount != 0)
             {
                 ShootShotgun();
                 timeOfLastShot=Time.time;
+                itemBehaviour.ammoCount--;
                 isFiring = false;
             }
         }
-        else if (GunType == "pistol")
+        else if (currentItem == "pistol")
         {
             timeBetweenShots = 0.5;
-            if (Input.GetButtonDown("Fire1") && Time.time - timeOfLastShot > timeBetweenShots)
+            if (Input.GetButtonDown("Fire1") && Time.time - timeOfLastShot > timeBetweenShots &&  itemBehaviour.ammoCount > 0)
             {
                 Shoot();
                 timeOfLastShot=Time.time;
+                itemBehaviour.ammoCount--;
                 isFiring = false;
             }
         }
-        else if (GunType == "ar")
+        else if (currentItem == "ar")
         {
             timeBetweenShots = 0.1;
-            if (Input.GetButton("Fire1") && Time.time - timeOfLastShot > timeBetweenShots)
+            if (Input.GetButton("Fire1") && Time.time - timeOfLastShot > timeBetweenShots &&  itemBehaviour.ammoCount > 0)
             {
                 Shoot();
                 timeOfLastShot=Time.time;
+                itemBehaviour.ammoCount--;
                 isFiring = false;
             }
         }
